5e83733712d0c19f7df983572e700da92cb7e5eb
sina dehvari <cnadhv7@gmail.com>
2023-11-28 15:58:09 +0330

    Update projectsdata.js
    update the data for projects section

---
+++ src/pages/projectsdata.js
@@ -0,0 +1,25 @@
+const projectsdata = [
+ {
+  title : "SHEYSHOP",
+  image :"./online-shopping-landing-page_52683-5991.jpg",
+  description :
+  "An ecommerce application developed using react , node , redux and mongoDB",
+  link :"/",
+ } ,
+ {
+  title : "SHEYPIZZA",
+  image :"./pizza-restaurant-landing-page_23-2148718475.jpg",
+  description :
+  "An ecommerce application developed using react , node , redux and mongoDB",
+  link :"/",
+ },
+ {
+  title : "SHEYROOM",
+  image :"./organic-flat-hotel-landing-page-illustrated_23-2148889921.jpg",
+  description :
+  "An ecommerce application developed using react , node , redux and mongoDB",
+  link :"/",
+ }
+]
+
+export default projectsdata
