6b63f59e65c1e77947dbff731d2da7e320dc089c
sina dehvari <cnadhv7@gmail.com>
2023-11-28 15:59:08 +0330

    Update servicesdata.js

---
+++ src/pages/servicesdata.js
@@ -0,0 +1,42 @@
+const services = [
+ {
+  title : "Design a modern Website",
+  image : "https://img.freepik.com/free-photo/online-web-design_53876-95309.jpg?w=740&t=st=1700394118~exp=1700394718~hmac=81cf9f3401810d255ceec8fc52f21edd5d715cd312b8f02da08c0808f2ca1016",
+  link : "/",
+ } ,
+
+ {
+  title : "Good SEO web",
+  image : "https://img.freepik.com/premium-photo/person-manage-search-engine-optimization-seo-digital-marketing-with-social-media-content_34141-1044.jpg?w=826",
+  link : "/",
+ } ,
+
+ {
+  title : "fix your website code bug",
+  image : "https://img.freepik.com/free-photo/smiling-african-american-freelancer-working-remote-typing-looking-computer-screen-while-roommate-is-relaxing-couch-male-programmer-using-pc-develop-software-home-reading-emails_482257-64568.jpg?w=740&t=st=1700394357~exp=1700394957~hmac=8637d8466a5ca711b0735801eb1a4da3770c8c2641da830f1a3fd2dc1f985e42",
+  link : "/",
+ } ,
+
+ {
+  title : "UI/UX Design",
+  image : "https://img.freepik.com/premium-photo/team-working-interface-design_274689-38849.jpg?w=740",
+  link : "/",
+ } ,
+
+ {
+  title : "Web Design with Wordpress",
+  image : "https://img.freepik.com/free-photo/web-design-internet-website-responsive-software-concept_53876-124757.jpg?w=826&t=st=1700394554~exp=1700395154~hmac=2ad3b115ce18296237726835e0bc7ec275c2440773a13e5286add27883b224f7",
+  link : "/",
+ } ,
+
+ {
+  title : "Design a Android Application",
+  image : "https://img.freepik.com/free-photo/representations-user-experience-interface-design_23-2150038917.jpg?w=740&t=st=1700394592~exp=1700395192~hmac=722f09b9e651a9b85f6eafdcd6e4d47bf34264f194731891a3e7b45817986f0b",
+  link : "/",
+ } ,
+
+ 
+
+]
+
+export default services;
